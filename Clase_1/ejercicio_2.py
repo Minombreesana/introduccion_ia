de8ac0f97a64863ae5298afdcdd5be3e627c5b5d
Minombreesana <apguzmanc@gmail.com>
2022-03-22 18:22:00 -0500

    sumé unos prints al ejercicio 2 clase 1

--- Clase_1/ejercicio_2.py
+++ Clase_1/ejercicio_2.py
@@ -6,13 +6,15 @@ class Sorting:
         self.array = array
 
     def indices(self):
-        id = np.argsort((np.sum((abs(self.array))**2, axis=1))**0.5)[::-1]
-        print(id)
+        norma_l2 = np.sum((abs(self.array))**2, axis=1)**0.5
+        id = np.argsort(norma_l2)[::-1]
+        print("Las normas de las filas son:",norma_l2)
+        print("Los indices de las normas ordenados descendente: ",id)
         return self.array[id]
         
 matriz = Sorting(np.array([[2,4,0,3],[10,23,1,8],[0,3,7,9]]))
 
-print(matriz.indices())
+print("La matriz ordenada de norma l2 mayor a menor: \n", matriz.indices())
         
     
         
