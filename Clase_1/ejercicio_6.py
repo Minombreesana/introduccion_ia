8e0697b3ece07d28fc5cfaf15b1cf63af01098cf
Minombreesana <apguzmanc@gmail.com>
2022-04-01 15:00:50 -0500

    corregí la dimensión de los centroids

--- Clase_1/ejercicio_6.py
+++ Clase_1/ejercicio_6.py
@@ -5,16 +5,14 @@ class Distance:
     def __init__(self, array):
         self.array = array
     
-    def distance_centroides(self, centroides):
-        distance_x_1 = np.sum((centroides.array - self.array[0])**2, axis = 1)**0.5 
-        distance_x_2 = np.sum((centroides.array - self.array[1])**2, axis = 1)**0.5 
-        distance_x_3 = np.sum((centroides.array - self.array[2])**2, axis = 1)**0.5
+    def distance_centroides(self, centroids):
 
-        distances = np.array([distance_x_1, distance_x_2,distance_x_3])
+        centroids = centroids.array[:,None]
+        distances = np.sum((centroids - self.array)**2, axis = 2)**0.5 
 
         return distances
 
 x = Distance(np.array([(1,2,3),(4,5,6),(7,8,9)]))
-centroides = Distance(np.array([(1,0,0),(0,1,1)])) 
+centroids = Distance(np.array([(1,0,0),(0,1,1)])) 
 
-print(f'Las distancias de los vectores x a los centroides son:{x.distance_centroides(centroides)}')
+print(f'Las distancias de los vectores x a los centroides son:{x.distance_centroides(centroids)}')
--- Clase_1/ejercicio_7.py
+++ Clase_1/ejercicio_7.py
@@ -5,23 +5,14 @@ class Distance:
     def __init__(self, array):
         self.array = array
     
-    def distances(self, centroides):
-        distance_x_1 = np.sum((centroides.array - self.array[0])**2, axis = 1)**0.5 
-        distance_x_2 = np.sum((centroides.array - self.array[1])**2, axis = 1)**0.5 
-        distance_x_3 = np.sum((centroides.array - self.array[2])**2, axis = 1)**0.5
+    def min_distances(self, centroids):
+        centroids = centroids.array[:,None]
+        distances = np.sum((centroids - self.array)**2, axis = 2)**0.5 
+        min_distances = np.argmin(distances, axis = 0)
 
-        distances = np.array([distance_x_1, distance_x_2,distance_x_3])
-
-        return distances
-
-    def min_distances(self, distances):
-        min_distances = np.argmin(distances, axis = 1)
         return min_distances
     
-
 x = Distance(np.array([(1,2,3),(4,5,6),(7,8,9)]))
-centroides = Distance(np.array([(1,0,0),(0,1,1)])) 
-distances = x.distances(centroides)
+centroids = Distance(np.array([(1,0,0),(0,1,1)])) 
 
-print(f'Las distancias de los vectores x a los centroides son:{x.distances(centroides)}')
-print(f'Los centroides a los que perteneces cada vector x son:{x.min_distances(distances)}')
+print(f'Los centroides a los que pertenece cada vector x son:{x.min_distances(centroids)}')
