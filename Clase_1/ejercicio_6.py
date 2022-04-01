da2ec8f6c702d61ec7867f5d65890f9a3f43fbb6
Minombreesana <apguzmanc@gmail.com>
2022-04-01 13:53:27 -0500

    corregí un centroide que estaba mal tipeado

--- Clase_1/ejercicio_6.py
+++ Clase_1/ejercicio_6.py
@@ -13,6 +13,6 @@ class Distance:
         return distance_x_1, distance_x_2, distance_x_3
 
 x = Distance(np.array([(1,2,3),(4,5,6),(7,8,9)]))
-centroides = Distance(np.array([(1,0,0),(0,0,1)])) 
+centroides = Distance(np.array([(1,0,0),(0,1,1)])) 
 
 print(f'Las distancias de los vectores x a los centroides son:{x.distance_centroides(centroides)}')
