084abb5b6245adab203533bfe740d0ad5a625bce
Minombreesana <apguzmanc@gmail.com>
2022-04-18 17:33:20 -0500

    corregí el 7 también

--- Clase_1/ejercicio_7.py
+++ Clase_1/ejercicio_7.py
@@ -2,17 +2,20 @@ import numpy as np
 
 class Distance:
 
-    def __init__(self, array):
+    def __init__(self, array, centroids):
         self.array = array
+        self.centroids = centroids
     
-    def min_distances(self, centroids):
-        centroids = centroids.array[:,None]
+    def min_distances(self):
+        centroids = self.centroids[:,None]
         distances = np.sum((centroids - self.array)**2, axis = 2)**0.5 
         min_distances = np.argmin(distances, axis = 0)
 
         return min_distances
-    
-x = Distance(np.array([(1,2,3),(4,5,6),(7,8,9)]))
-centroids = Distance(np.array([(1,0,0),(0,1,1)])) 
 
+vector = np.array([(1,2,3),(4,5,6),(7,8,9)])
+centroids = np.array([(1,0,0),(0,1,1)])
+
+min_distance = Distance(vector, centroids)
+ 
 print(f'Los centroides a los que pertenece cada vector x son:{x.min_distances(centroids)}')
